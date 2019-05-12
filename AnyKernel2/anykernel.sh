edc9b7f290aa69b76b56c857f0995de11e27631f
gandalf02 <pradeepkumarmusham@gmail.com>
2019-05-12 07:54:59 +0000

    anykernel modifications for unified builds

--- AnyKernel2/anykernel.sh
+++ AnyKernel2/anykernel.sh
@@ -10,10 +10,10 @@ do.modules=0
 do.cleanup=1
 do.cleanuponabort=0
 device.name1=santoni
-device.name2=
-device.name3=
-device.name4=
-device.name5=
+device.name2=Xiaomi
+device.name3=Redmi 4X
+supported.sdk1=27
+supported.sdk2=28
 '; } # end properties
 
 # shell variables
@@ -29,8 +29,9 @@ ramdisk_compression=auto;
 
 ## AnyKernel file attributes
 # set permissions/ownership for included ramdisk files
-chmod -R 750 $ramdisk/*;
-chown -R root:root $ramdisk/*;
+# set permissions for included ramdisk files
+chmod -R 755 $ramdisk
+chmod +x $ramdisk/sbin/spa
 
 
 ## AnyKernel install
@@ -38,26 +39,15 @@ dump_boot;
 
 # begin ramdisk changes
 
-# init.rc
-#backup_file init.rc;
-#replace_string init.rc "cpuctl cpu,timer_slack" "mount cgroup none /dev/cpuctl cpu" "mount cgroup none /dev/cpuctl cpu,timer_slack";
-#append_file init.rc "run-parts" init;
-
-# init.tuna.rc
-#backup_file init.tuna.rc;
-#insert_line init.tuna.rc "nodiratime barrier=0" after "mount_all /fstab.tuna" "\tmount ext4 /dev/block/platform/omap/omap_hsmmc.0/by-name/userdata /data remount nosuid nodev noatime nodiratime barrier=0";
-#append_file init.tuna.rc "dvbootscript" init.tuna;
-
-# fstab.tuna
-#backup_file fstab.tuna;
-#patch_fstab fstab.tuna /system ext4 options "noatime,barrier=1" "noatime,nodiratime,barrier=0";
-#patch_fstab fstab.tuna /cache ext4 options "barrier=1" "barrier=0,nomblk_io_submit";
-#patch_fstab fstab.tuna /data ext4 options "data=ordered" "nomblk_io_submit,data=writeback";
-#append_file fstab.tuna "usbdisk" fstab;
-
+# fstab.qcom
+if [ -f /fstab.qcom ]; then
+insert_line fstab.qcom "data        f2fs" before "data        ext4" "/dev/block/bootdevice/by-name/userdata     /data        f2fs    nosuid,nodev,noatime,inline_xattr,data_flush      wait,check,encryptable=footer,formattable,length=-16384";
+insert_line fstab.qcom "cache        f2fs" after "data        ext4" "/dev/block/bootdevice/by-name/cache     /cache        f2fs    nosuid,nodev,noatime,inline_xattr,flush_merge,data_flush wait,formattable,check";
+fi;
+# backup_file init.rc;
+	
 # end ramdisk changes
 
 write_boot;
 
 ## end install
-
